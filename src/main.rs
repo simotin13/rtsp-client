9dc580c3b4cd433b67261228f8fc1c1432b3e26a
Hiroyuki Miyazaki <miyazaki@mcommit.com>
2024-03-05 00:36:33 +0900

    WIP move rtsp client code to rtsp_client.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
-extern crate base64;
-use std::env;
-use std::io::BufRead;
-use std::io::{Write, BufReader, BufWriter};
-use std::net::TcpStream;
-use url::Url;
-use std::collections::HashMap;
+mod rtsp_client;
 use std::process;
+use std::env;
 
 fn main() {
-    const USER_AGENT: &str = "my-rtsp-client";
-
     let args: Vec<String> = env::args().collect();
-
     if args.len() < 2 {
         eprintln!("Usage. rtsp-client <rtsp url>");
         std::process::exit(1);
@@ -19,281 +11,28 @@ fn main() {
 
     let rtsp_url = &args[1];
 
-    // check if url starts with rtsp:// or rtspt://
-    if !rtsp_url.starts_with("rtsp://") && !rtsp_url.starts_with("rtspt://") {
-        eprintln!("rtsp_url does not start with rtsp:// or rtspt://");
-        std::process::exit(1);
-    }
-
-    let tmp = rtsp_url.split("://").collect::<Vec<&str>>()[1];
-    if !tmp.contains("@") {
-        eprintln!("rtsp_url does not contain @");
-        std::process::exit(1);
-    }
-    let tmp = tmp.split("@").collect::<Vec<&str>>()[0];
-    // check if url contains :
-    if !tmp.contains(":") {
-        eprintln!("rtsp_url does not contain : for username:password");
-        std::process::exit(1);
-    }
-
-    // get username and password
-    let pair = tmp.split(":").collect::<Vec<&str>>();
-    let username = pair[0];
-    let password = pair[1];
-
-    // get url,port
-    let url = match Url::parse(rtsp_url) {
-        Ok(url) => url,
-        Err(_) => {
-            eprintln!("invalid url:{}", rtsp_url);
+    let mut rtsp_client = match rtsp_client::RTSPClient::new(rtsp_url.to_string()) {
+        Ok(c) => c,
+        Err(e) => {
+            eprintln!("failed to create RTSPClient: {}", e);
             process::exit(1);
         }
     };
 
-    let host = match url.host_str() {
-        Some(host) => host.to_string(),
-        None => {
-            eprintln!("invalid url:{}", rtsp_url);
-            process::exit(1);
-        }
-    };
+    // OPTIONS
+    rtsp_client.options().expect("failed to send OPTIONS request");
 
-    let mut port = 554;
-    if let Some(p) = url.port() {
-        port = p;
-    }
-    let stream = TcpStream::connect(&format!("{}:{}",host, port)).expect(&format!("failed to connect server {}:{}, url", host, port));
-    let mut writer = BufWriter::new(&stream);
-
-    println!("connected to rtsp server {}:{}", host, port);
-    // send OPTIONS
-    let mut c_seq = 1;
-    let mut request = String::new();
-    request += &format!("OPTIONS {} RTSP/1.0\r\n", rtsp_url);
-    let mut req_headers: HashMap<String, String> = HashMap::new();
-    req_headers.insert("CSeq".to_string(), c_seq.to_string());
-    req_headers.insert("User-Agent".to_string(), USER_AGENT.to_string());
-    for (key, value) in &req_headers {
-        request += &format!("{}: {}\r\n", key, value);
-    }
-    request+= "\r\n";
-
-    if let Err(e) = writer.write_all(request.as_bytes()) {
-        eprintln!("failed to send OPTION request: {}", e);
-        process::exit(1);
-    }
-    let _ = writer.flush();
-
-    let mut reader = BufReader::new(&stream);
-    loop {
-        let mut line = String::new();
-        match reader.read_line(&mut line) {
-            Ok(len) => {
-                println!("line[{}]", line.trim_end_matches("\r\n").to_string());
-                if len == 0 {
-                    break;
-                }
-                if line == "\r\n" {
-                    break;
-                }
-            },
-            Err(e) => {
-                eprintln!("failed to read_line {}", e);
-                break;
-            }
-        }
-    }
-
-    // ====================================================
     // DESCRIBE
-    // ====================================================
-    c_seq += 1;
-    let mut request = String::new();
-    request += &format!("DESCRIBE {} RTSP/1.0\r\n", rtsp_url);
+    rtsp_client.describe().expect("failed to send DESCRIBE request");
 
-    let mut req_headers: HashMap<String, String> = HashMap::new();
-    req_headers.insert("CSeq".to_string(), c_seq.to_string());
-    req_headers.insert("User-Agent".to_string(), USER_AGENT.to_string());
-    // basic authentication
-    let auth = base64::encode(&format!("{}:{}", username, password));
-    req_headers.insert("Authorization".to_string(), format!("Basic {}", auth));
-    for (key, value) in &req_headers {
-        request += &format!("{}: {}\r\n", key, value);
-    }
-    request+= "\r\n";
-    if let Err(e) = writer.write_all(request.as_bytes()) {
-        eprintln!("failed to send DESCRIBE request: {}", e);
-        process::exit(1);
-    }
-    let _ = writer.flush();
+    // SETUP track1
+    rtsp_client.setup_track1().expect("failed to send SETUP request for track1");
 
-    loop {
-        let mut line = String::new();
-        match reader.read_line(&mut line) {
-            Ok(len) => {
-                println!("line[{}]", line.trim_end_matches("\r\n").to_string());
-                if len == 0 {
-                    break;
-                }
-                if line == "\r\n" {
-                    break;
-                }
-            },
-            Err(e) => {
-                eprintln!("failed to read_line {}", e);
-                break;
-            }
-        }
-    }
-
-    // ====================================================
-    // SETUP
-    // ====================================================
-    c_seq += 1;
-    let mut request = String::new();
-    request += &format!("SETUP {}/track1 RTSP/1.0\r\n", rtsp_url);
-
-    let mut req_headers: HashMap<String, String> = HashMap::new();
-    req_headers.insert("CSeq".to_string(), c_seq.to_string());
-    req_headers.insert("User-Agent".to_string(), USER_AGENT.to_string());
-    let auth = base64::encode(&format!("{}:{}", username, password));
-    req_headers.insert("Authorization".to_string(), format!("Basic {}", auth));
-    // TODO: port number should be dynamic
-    req_headers.insert("Transport".to_string(), "RTP/AVP;unicast;client_port=56648-56649".to_string());
-    for (key, value) in &req_headers {
-        request += &format!("{}: {}\r\n", key, value);
-    }
-    request+= "\r\n";
-    if let Err(e) = writer.write_all(request.as_bytes()) {
-        eprintln!("failed to send SETUP request: {}", e);
-        process::exit(1);
-    }
-    let _ = writer.flush();
-
-    let mut session = String::new();
-    loop {
-        let mut line = String::new();
-        match reader.read_line(&mut line) {
-            Ok(len) => {
-                println!("line[{}]", line.trim_end_matches("\r\n").to_string());
-                
-                // check if session is in the response
-                let pair = line.split(":").collect::<Vec<&str>>();
-                if pair.len() == 2 {
-                    if pair[0].trim() == "Session" {
-                        session = pair[1].trim().to_string();
-                        session = session.split(";").collect::<Vec<&str>>()[0].to_string();
-                        println!("session:{}", session);
-                    }
-                }
-        
-                if len == 0 {
-                    break;
-                }
-                if line == "\r\n" {
-                    break;
-                }
-            },
-            Err(e) => {
-                eprintln!("failed to read_line {}", e);
-                break;
-            }
-        }
-    }
-
-    // ====================================================
     // SETUP track2
-    // ====================================================
-    c_seq += 1;
-    let mut request = String::new();
-    request += &format!("SETUP {}/track2 RTSP/1.0\r\n", rtsp_url);
-    let mut req_headers: HashMap<String, String> = HashMap::new();
-    req_headers.insert("CSeq".to_string(), c_seq.to_string());
-    req_headers.insert("User-Agent".to_string(), USER_AGENT.to_string());
-    let auth = base64::encode(&format!("{}:{}", username, password));
-    req_headers.insert("Authorization".to_string(), format!("Basic {}", auth));
-    req_headers.insert("Session".to_string(), session.clone());
+    rtsp_client.setup_track2().expect("failed to send SETUP request for track2");
 
-    // TODO: port number should be dynamic
-    req_headers.insert("Transport".to_string(), "RTP/AVP;unicast;client_port=56648-56649".to_string());
-    for (key, value) in &req_headers {
-        request += &format!("{}: {}\r\n", key, value);
-    }
-    request+= "\r\n";
-    if let Err(e) = writer.write_all(request.as_bytes()) {
-        eprintln!("failed to send SETUP request: {}", e);
-        process::exit(1);
-    }
-    let _ = writer.flush();
-
-    let lines:Vec<String> = Vec::new();
-    loop {
-        let mut line = String::new();
-        match reader.read_line(&mut line) {
-            Ok(len) => {
-                println!("line[{}]", line.trim_end_matches("\r\n").to_string());
-                if len == 0 {
-                    break;
-                }
-                if line == "\r\n" {
-                    break;
-                }
-            },
-            Err(e) => {
-                eprintln!("failed to read_line {}", e);
-                break;
-            }
-        }
-    }
-    for line in &lines {
-        println!("{}", line);
-    }
-
-    // ====================================================
     // PLAY
-    // ====================================================
-    c_seq += 1;
-    let mut request = String::new();
-    request += &format!("PLAY {} RTSP/1.0\r\n", rtsp_url);
-    let mut req_headers: HashMap<String, String> = HashMap::new();
-    req_headers.insert("CSeq".to_string(), c_seq.to_string());
-    req_headers.insert("User-Agent".to_string(), USER_AGENT.to_string());
-    let auth = base64::encode(&format!("{}:{}", username, password));
-    req_headers.insert("Authorization".to_string(), format!("Basic {}", auth));
-    req_headers.insert("Session".to_string(), session);
-    req_headers.insert("Range".to_string(), "npt=0.000-".to_string());
-    for (key, value) in &req_headers {
-        request += &format!("{}: {}\r\n", key, value);
-    }
-    request+= "\r\n";
-    if let Err(e) = writer.write_all(request.as_bytes()) {
-        eprintln!("failed to send Play request: {}", e);
-        process::exit(1);
-    }
-    let _ = writer.flush();
+    rtsp_client.play().expect("failed to send PLAY request");
 
-    loop {
-        let mut line = String::new();
-        match reader.read_line(&mut line) {
-            Ok(len) => {
-                println!("line[{}]", line.trim_end_matches("\r\n").to_string());
-                if len == 0 {
-                    break;
-                }
-                if line == "\r\n" {
-                    break;
-                }
-            },
-            Err(e) => {
-                eprintln!("failed to read_line {}", e);
-                break;
-            }
-        }
-    }
-    for line in &lines {
-        println!("{}", line);
-    }
-
-    stream.shutdown(std::net::Shutdown::Both).unwrap();
+    rtsp_client.shutdown();
 }
---
+++ src/rtsp_client.rs
@@ -0,0 +1,308 @@
+extern crate base64;
+use std::io::Write;
+use std::io::BufRead;
+use std::io::{BufReader, BufWriter};
+use std::net::TcpStream;
+use url::Url;
+use std::collections::HashMap;
+
+pub struct RTSPClient {
+    user_agent: &'static str,
+    rtsp_url: String,
+    username: String,
+    password: String,
+    c_seq: u32,
+    session: String,
+    stream: TcpStream,
+}
+
+impl RTSPClient {
+    pub fn new(rtsp_url: String) -> Result<RTSPClient, String> {
+        // check if url starts with rtsp:// or rtspt://
+        if !rtsp_url.starts_with("rtsp://") && !rtsp_url.starts_with("rtspt://") {
+            return Err("URL must start with rtsp:// or rtspt://".to_string());
+        }
+        let tmp = rtsp_url.split("://").collect::<Vec<&str>>()[1];
+        if !tmp.contains("@") {
+            return Err("rtsp_url does not contain @".to_string());
+        }
+        let tmp = tmp.split("@").collect::<Vec<&str>>()[0];
+        // check if url contains :
+        if !tmp.contains(":") {
+            return Err("rtsp_url does not contain : for username:password".to_string());
+        }
+
+        // get username and password
+        let pair = tmp.split(":").collect::<Vec<&str>>();
+        let username = pair[0];
+        let password = pair[1];
+
+        // get url,port
+        let url = match Url::parse(&rtsp_url) {
+            Ok(u) => u,
+            Err(e) => return Err(format!("failed to parse url: {}", e)),
+        };
+
+        let host = match url.host_str() {
+            Some(h) => h,
+            None => return Err(format!("failed to parse url: {}", url)),
+        };
+
+        let mut port = 554;
+        if let Some(p) = url.port() {
+            port = p;
+        };
+
+        // connect to RTSP server, if failed, return Err
+        let stream = match TcpStream::connect(&format!("{}:{}", host, port)) {
+            Ok(s) => s,
+            Err(_) => return Err(format!("failed to connect to {}:{}", host, port)),
+        };
+    
+        Ok(RTSPClient {
+            user_agent: "my-rtsp-client",
+            rtsp_url: rtsp_url.to_string(),
+            username: username.to_string(),
+            password: password.to_string(),
+            session: String::new(),
+            c_seq: 0,
+            stream: stream.try_clone().unwrap(),
+        })
+    }
+
+    pub fn options(&mut self) -> Result<(), String> {
+        self.c_seq += 1;
+        let mut request = String::new();
+        request += &format!("OPTIONS {} RTSP/1.0\r\n", self.rtsp_url);
+        let mut req_headers: HashMap<String, String> = HashMap::new();
+        req_headers.insert("CSeq".to_string(), self.c_seq.to_string());
+        req_headers.insert("User-Agent".to_string(), self.user_agent.to_string());
+        for (key, value) in &req_headers {
+            request += &format!("{}: {}\r\n", key, value);
+        }
+        request+= "\r\n";
+   
+        let mut writer = BufWriter::new(&self.stream);
+        if let Err(e) = writer.write_all(request.as_bytes()) {
+            return Err(format!("failed to send OPTIONS request: {}", e));
+        }
+        let _ = writer.flush();
+
+        let mut reader = BufReader::new(&self.stream);
+        loop {
+            let mut line = String::new();
+            match reader.read_line(&mut line) {
+                Ok(len) => {
+                    println!("line[{}]", line.trim_end_matches("\r\n").to_string());
+                    if len == 0 {
+                        break;
+                    }
+                    if line == "\r\n" {
+                        break;
+                    }
+                },
+                Err(e) => {
+                    return Err(format!("failed to read_line: {}", e));
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn describe(&mut self) -> Result<(), String> {
+        self.c_seq += 1;
+        let mut request = String::new();
+        request += &format!("DESCRIBE {} RTSP/1.0\r\n", self.rtsp_url);
+
+        let mut req_headers: HashMap<String, String> = HashMap::new();
+        req_headers.insert("CSeq".to_string(), self.c_seq.to_string());
+        req_headers.insert("User-Agent".to_string(), self.user_agent.to_string());
+
+        // basic authentication
+        let auth = base64::encode(&format!("{}:{}", self.username, self.password));
+        req_headers.insert("Authorization".to_string(), format!("Basic {}", auth));
+        for (key, value) in &req_headers {
+            request += &format!("{}: {}\r\n", key, value);
+        }
+        request+= "\r\n";
+
+        let mut writer = BufWriter::new(&self.stream);
+        if let Err(e) = writer.write_all(request.as_bytes()) {
+            eprintln!("failed to send DESCRIBE request: {}", e);
+            return Err(format!("failed to send DESCRIBE request: {}", e));
+        }
+        let _ = writer.flush();
+
+        let mut reader = BufReader::new(&self.stream);
+        loop {
+            let mut line = String::new();
+            match reader.read_line(&mut line) {
+                Ok(len) => {
+                    println!("line[{}]", line.trim_end_matches("\r\n").to_string());
+                    if len == 0 {
+                        break;
+                    }
+                    if line == "\r\n" {
+                        break;
+                    }
+                },
+                Err(e) => {
+                    return Err(format!("failed to read_line: {}", e));
+                }
+            }
+        }
+    
+        Ok(())
+    }
+
+    pub fn setup_track1(&mut self) -> Result<(), String> {
+        self.c_seq += 1;
+        let mut request = String::new();
+        request += &format!("SETUP {}/track1 RTSP/1.0\r\n", self.rtsp_url);
+
+        let mut req_headers: HashMap<String, String> = HashMap::new();
+        req_headers.insert("CSeq".to_string(), self.c_seq.to_string());
+        req_headers.insert("User-Agent".to_string(), self.user_agent.to_string());
+        let auth = base64::encode(&format!("{}:{}", self.username, self.password));
+        req_headers.insert("Authorization".to_string(), format!("Basic {}", auth));
+        // TODO: port number should be dynamic
+        req_headers.insert("Transport".to_string(), "RTP/AVP;unicast;client_port=56648-56649".to_string());
+        for (key, value) in &req_headers {
+            request += &format!("{}: {}\r\n", key, value);
+        }
+        request+= "\r\n";
+
+        let mut writer = BufWriter::new(&self.stream);
+        if let Err(e) = writer.write_all(request.as_bytes()) {
+            return Err(format!("failed to send SETUP request: {}", e));
+        }
+        let _ = writer.flush();
+
+        let mut reader = BufReader::new(&self.stream);
+        loop {
+            let mut line = String::new();
+            match reader.read_line(&mut line) {
+                Ok(len) => {
+                    println!("line[{}]", line.trim_end_matches("\r\n").to_string());
+                    
+                    // check if session is in the response
+                    let pair = line.split(":").collect::<Vec<&str>>();
+                    if pair.len() == 2 {
+                        if pair[0].trim() == "Session" {
+                            let tmp = pair[1].trim().to_string();
+                            let session = tmp.split(";").collect::<Vec<&str>>()[0].to_string();
+                            self.session = session;
+                            println!("session:{}", self.session);
+                        }
+                    }
+            
+                    if len == 0 {
+                        break;
+                    }
+                    if line == "\r\n" {
+                        break;
+                    }
+                },
+                Err(e) => {
+                    return Err(format!("failed to read_line: {}", e));
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn setup_track2(&mut self) -> Result<(), String> {
+        self.c_seq += 1;
+        let mut request = String::new();
+        request += &format!("SETUP {}/track2 RTSP/1.0\r\n", self.rtsp_url);
+        let mut req_headers: HashMap<String, String> = HashMap::new();
+        req_headers.insert("CSeq".to_string(), self.c_seq.to_string());
+        req_headers.insert("User-Agent".to_string(), self.user_agent.to_string());
+        let auth = base64::encode(&format!("{}:{}", self.username, self.password));
+        req_headers.insert("Authorization".to_string(), format!("Basic {}", auth));
+        req_headers.insert("Session".to_string(), self.session.clone());
+    
+        // TODO: port number should be dynamic
+        req_headers.insert("Transport".to_string(), "RTP/AVP;unicast;client_port=56648-56649".to_string());
+        for (key, value) in &req_headers {
+            request += &format!("{}: {}\r\n", key, value);
+        }
+        request+= "\r\n";
+
+        let mut writer = BufWriter::new(&self.stream);
+        if let Err(e) = writer.write_all(request.as_bytes()) {
+            return Err(format!("failed to send SETUP request: {}", e));
+        }
+        let _ = writer.flush();
+
+        let mut reader = BufReader::new(&self.stream);
+        loop {
+            let mut line = String::new();
+            match reader.read_line(&mut line) {
+                Ok(len) => {
+                    println!("line[{}]", line.trim_end_matches("\r\n").to_string());
+                    if len == 0 {
+                        break;
+                    }
+                    if line == "\r\n" {
+                        break;
+                    }
+                },
+                Err(e) => {
+                    return Err(format!("failed to read_line: {}", e));
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn play(&mut self) -> Result<(), String> {
+        self.c_seq += 1;
+        let mut request = String::new();
+        request += &format!("PLAY {} RTSP/1.0\r\n", self.rtsp_url);
+        let mut req_headers: HashMap<String, String> = HashMap::new();
+        req_headers.insert("CSeq".to_string(), self.c_seq.to_string());
+        req_headers.insert("User-Agent".to_string(), self.user_agent.to_string());
+        let auth = base64::encode(&format!("{}:{}", self.username, self.password));
+        req_headers.insert("Authorization".to_string(), format!("Basic {}", auth));
+        req_headers.insert("Session".to_string(), self.session.clone());
+        req_headers.insert("Range".to_string(), "npt=0.000-".to_string());
+        for (key, value) in &req_headers {
+            request += &format!("{}: {}\r\n", key, value);
+        }
+        request+= "\r\n";
+
+        let mut writer = BufWriter::new(&self.stream);
+        if let Err(e) = writer.write_all(request.as_bytes()) {
+            return Err(format!("failed to send PLAY request: {}", e));
+        }
+        let _ = writer.flush();
+
+        let mut reader = BufReader::new(&self.stream);
+        loop {
+            let mut line = String::new();
+            match reader.read_line(&mut line) {
+                Ok(len) => {
+                    println!("line[{}]", line.trim_end_matches("\r\n").to_string());
+                    if len == 0 {
+                        break;
+                    }
+                    if line == "\r\n" {
+                        break;
+                    }
+                },
+                Err(e) => {
+                    return Err(format!("failed to read_line: {}", e));
+                }
+            }
+        }
+        Ok(())
+    }
+
+    pub fn shutdown(&mut self) {
+        self.stream.shutdown(std::net::Shutdown::Both).unwrap();
+    }
+}
